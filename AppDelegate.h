b4f8a660bc55c908db8a4e7652d50b3bc7d8a164
abhisheksaralaya <42572088+abhisheksaralaya@users.noreply.github.com>
2018-08-21 16:44:43 +0530

    Create AppDelegate.h

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sampleProject
+//
+//  Created by Abhishek on 08/08/18.
+//  Copyright © 2018 Abhishek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
