81085cb54a8352a48fc6de0ccb153896193ce307
abhisheksaralaya <42572088+abhisheksaralaya@users.noreply.github.com>
2018-08-21 16:59:36 +0530

    Create main.m

---
+++ main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  sampleProject
+//
+//  Created by Abhishek on 08/08/18.
+//  Copyright © 2018 Abhishek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
